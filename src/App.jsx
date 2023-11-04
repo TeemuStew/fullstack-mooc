6c6f2ea50bc86865ae80ac878a565b5fb73dcb8f
Teemu Mäkelä <the-makela@hotmail.com>
2023-11-04 18:40:27 +0200

    Tehtävä

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Button = ({ text, handleClick }) => {
-    return <button onClick={handleClick}>{text}</button>;
+const Header = ({ course }) => {
+    return <h1>{course}</h1>;
 };
 
-const StatisticLine = ({ text, value }) => {
+const Part = ({ part, exercise }) => {
     return (
         <p>
-            {text}: {value}
+            {part} {exercise}
         </p>
     );
 };
 
-const Statistics = ({ good, neutral, bad }) => {
-    const total = good + neutral + bad;
-    const average = total === 0 ? 0 : (good - bad) / total;
-    const positivePercentage = total === 0 ? 0 : (good / total) * 100;
-
+const Content = ({ parts }) => {
     return (
         <div>
-            <h2>Statistics</h2>
-            {total === 0 ? (
-                <p>No feedback given</p>
-            ) : (
-                <div>
-                    <StatisticLine text="Good" value={good} />
-                    <StatisticLine text="Neutral" value={neutral} />
-                    <StatisticLine text="Bad" value={bad} />
-                    <StatisticLine text="Total" value={total} />
-                    <StatisticLine text="Average" value={average.toFixed(2)} />
-                    <StatisticLine text="Positive" value={`${positivePercentage.toFixed(1)}%`} />
-                </div>
-            )}
+            <Part part={parts[0].name} exercise={parts[0].exercises} />
+            <Part part={parts[1].name} exercise={parts[1].exercises} />
+            <Part part={parts[2].name} exercise={parts[2].exercises} />
         </div>
     );
 };
-
-const App = () => {
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+/*
+const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
 
     return (
-        <div>
-            <h1>Give Feedback</h1>
-            <Button text="Good" handleClick={() => setGood(good + 1)} />
-            <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
-            <Button text="Bad" handleClick={() => setBad(bad + 1)} />
+        <p>
+            Number of exercises {totalExercises}
+        </p>
+    );
+}; */
 
-            <Statistics good={good} neutral={neutral} bad={bad} />
+const App = () => {
+    const course = 'Half Stack application development';
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ];
+
+    // <Total parts={parts} />       Content parts alla
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} />
         </div>
     );
 };
